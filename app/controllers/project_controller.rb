efbe811d80a27d3a35c0b55937198008c83c5ebc
Irmak <nihal.pakis@mail.mcgill.ca>
2020-05-24 18:50:47 -0400

    Authentication Implemented

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,9 +1,16 @@
 class ProjectController < ApplicationController
-  
-  def login
-  end
-  
-  def main
+  protect_from_forgery with: :exception
+  before_action :require_login
+
+  def index
+    @user = current_user
   end
 
+  private 
+
+  def require_login
+    if current_user.nil?
+      redirect_to '/users/sign_in'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'project/login'
-  get '/' => 'project#login'
-  get '/index' => 'project#index'
-
+  
+  root to: 'project#index'
+  
+  devise_for :users
+  
+  # get '/login' => 'project#login'
+  # get '/index' => 'project#index'
+  # get '/register' => 'project#register'
+  # get '/forgetPassword' => 'project#forgetPassword'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
